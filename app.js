4a3ca0716a957e5ea05575c2984d7be2d550e8cb
maxhar518 <mazharalib518@gmail.com>
2024-02-07 23:54:24 +0500

    first commit

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 
 const memberInput = document.querySelector("#member")
 const container = document.querySelector("#container")
+const highestVoteMemberDiv = document.querySelector("#highestVoteMember")
 
 const membersData = []
 
 const loopHandler = () => {
     container.innerHTML = ""
     let i = 0
+    let highestVote = 0
+    let highestVoteIndex = -1
+
     for (let item of membersData) {
         container.innerHTML +=
             `
@@ -14,24 +18,31 @@ const loopHandler = () => {
             <span> vote: ${item.vote}</span>
             <button onclick="voteHandler(${i})" >Increase Vote</button>
             <button onclick="edit(this)">Edit</button>
-            <button onclick="del(this)">Delete</button>
+            <button onclick="del(this)">Delete</button><br>
             </br></br>
              `
+             if(item.vote > highestVote){
+                highestVote = item.vote
+                highestVoteIndex =i
+             }
         i = i + 1
+    }
+
+    if(highestVoteIndex !==-1){
+        highestVoteMemberDiv.textContent = `Highest Votes:(${membersData[highestVoteIndex].name})`
+    }else{
+        highestVoteMemberDiv.textContent = ''
     }
 }
 
 const voteHandler = (index) => {
-    console.log(index);
-    console.log(membersData[index]);
-    membersData[index].vote += 1
+    var a = membersData[index].vote += 1
     loopHandler()
+    // console.log(a);
 }
 
 
 const submitHandler = () => {
-    console.log("I am submit handler");
-
     const member = {
         name: memberInput.value,
         vote: 0
